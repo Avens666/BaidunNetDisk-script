46eec9ab79238fc72bd9634fc1fafd6334664eff
Evan Chen <56132958@qq.com>
2020-10-27 18:48:51 +0800

    Update 百度网盘_文件库清单导出.user.js
     2020.10.26-V0.5 增加取子目录的功能

--- 百度网盘_文件库清单导出.user.js
+++ 百度网盘_文件库清单导出.user.js
@@ -1,14 +1,21 @@
 // ==UserScript==
 // @name         百度网盘共享文件库目录清单导出
 // @namespace    https://github.com/Avens666/BaidunNetDisk-script
-// @version      0.3
+// @version      0.5
 // @description  try to take over the world!
 // @author       Avens
 // @match        https://pan.baidu.com/mbox*
 // @grant        none
 // @require      https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/1.3.3/FileSaver.min.js
-// @require      https://code.jquery.com/jquery-3.1.1.min.js
+// @require      https://cdn.bootcdn.net/ajax/libs/jquery/3.1.1/jquery.min.js
+// @note         2020.10.26-V0.5 增加取子目录的功能
 // ==/UserScript==
+
+//
+// 主目录类型 type1 <li class="" node-type="sharelist-item" data-id="94956815229992845" data-frm="2687769004" data-to="2181389256" data-fid="1117071517154765">
+// 主目录类型 type2 <li class="" node-type="sharelist-item" data-id="8522742650637316248" data-frm="574440818" data-gid="697250947953272568" data-fid="494436556508384">
+// 子目录 <li class="" node-type="sharelist-item" data-fid="715901954634145">
+
 
 (function() {
     'use strict';
@@ -17,176 +24,156 @@
     'title': 'sharelist-view-toggle',
   };
     var g_type=0;
+    var g_gid;
+    var g_msgid;
+    var g_frm;
+    var g_to_uk;
 
-    function exportDirInfo(){
+    //获取主目录信息
+ function getGID(){
      //遍历当前显示的目录及文件信息
-        //step1 取得gid 一个分享文件库的主ID  有两种type，一种使用form_uk 加gid查询 这是type2，一种使用 form_uk 加 to_uk查询,这是type1
-        var gid, _frm, _to;
-        var obj =  document.getElementsByTagName("li");
-        var obj_length = obj.length;
-        var frm_arr = [];
-        var to_arr = [];
-        var selected_arr = [];
-        var dirinfo = []; //一级列表数组
-        for(var i = 0 ; i < obj_length ; i++) {
-            //获取每个子元素的 id 值，1-10 之间的保留
-            //alert(tag_obj[i].getAttribute('id'));
-            var nodetype = obj[i].getAttribute("node-type");
-            if( nodetype && nodetype == "sharelist-item") {
-                var is_on = obj[i].getAttribute("class");
-                console.log("ison: "+ is_on);
-                selected_arr.push(is_on);
-            }
-            else
-            {
-                continue;
-            }
+     //step1 取得gid 一个分享文件库的主ID  有两种type，一种使用form_uk 加gid查询 这是type2; 一种使用 form_uk 加 to_uk查询,这是type1
+     let lst = Array.from(document.getElementsByTagName('li')).filter(item => item.getAttribute('node-type') === 'sharelist-item');
+     const obj = lst.filter(item => item.getAttribute('class') === 'on');
+     var obj_length = obj.length;
+     g_msgid = null;
+     if(obj_length != 1)
+     {
+         alert("请在文件库根目录下,仅选中一个目录后执行!" );
+         return;
+     }
 
-            _frm = obj[i].getAttributeNode("data-frm");
-            if( _frm ) {
-                frm_arr.push(_frm);
-            }
+     //判断是否目录
+     //<div class="sharelist-item-size global-ellipsis">-</div>
+     let lt = Array.from(obj[0].children).filter(item => item.getAttribute('class') === 'sharelist-item-size global-ellipsis');
+     if(lt.length == 1)
+     {
+         if(lt[0].textContent == '-')
+         {
+             g_msgid = obj[0].getAttribute('data-id');
+             g_gid = obj[0].getAttribute("data-gid");
+             g_frm = obj[0].getAttribute("data-frm");
+             g_to_uk = obj[0].getAttribute('data-to');
 
-            if( gid == undefined ) {
-                gid = obj[i].getAttributeNode("data-gid");
-                if(gid)  g_type = 2; //取得gid
-            }
+             if (g_to_uk === null) {
+                 g_type = 2;
+             } else {
+                 g_type = 1;
+             }
+         }
+     }
+     else
+     {
+         alert("请在文件库根目录下,仅选中一个目录后执行!" );
+         return;
+     }
 
-            _to = obj[i].getAttributeNode("data-to");
-            if( _to ) {
-                to_arr.push(_to);
-                g_type = 1;
-            }
-
-        }
-
-        if( g_type === 0) {
-            alert("请在文件库根目录下执行!" );
+    //    alert("g_gid=" + g_gid + "  msg_id=" + g_msgid+ " frm="+g_frm+ " to="+g_to_uk);
+        if( g_msgid === null) {
+            alert("请在文件库根目录下,选中一个目录后执行!" );
                return;
         }
-
-        //step2 根据gid获得第一级目录列表
-        var gid_url ="";
-        if(g_type == 2)
+        else
         {
-            // https://pan.baidu.com/mbox/group/listshare?gid=837398806018187810&limit=50&desc=1&type=2
-            gid_url = " https://pan.baidu.com/mbox/group/listshare?gid=" + gid.value + "&limit=50&desc=1&type=2";
-
-            $.ajax({
-                type:'GET',
-                url: gid_url,
-                data:{},
-                dataType: "json",
-                async: false,
-                success:function(res){
-                    //  var blob = new Blob(["Hello, world!"], {type: "text/plain;charset=utf-8"});
-                    //  saveAs(res, "hello.txt");
-                    if(res.errno != 0)
-                    {
-                        console.warn("errno:" + res.errno + "url: " + gid_url);
-                        return;
-                    }
-                    var msg_lst = res.records.msg_list;
-                    var obj_length = msg_lst.length;
-                    var info="" ;
-                    var index = 0;
-                    for(var i = 0 ; i < obj_length ; i++) {
-                        var file_lst = msg_lst[i].file_list;
-                        var _msg_id = msg_lst[i].msg_id;
-                        var len = file_lst.length;
-                        if( selected_arr[index]  == "on" )
-                        {//目录被选中才导出
-                            for(var n = 0 ; n < len ; n++) {
-    //                            console.log( file_lst[n].server_filename);
-                                var _name  = file_lst[n].server_filename;
-                                var _size  = file_lst[n].size;
-                                var _isDir = file_lst[n].isdir;
-                                var _fs_id = file_lst[n].fs_id;
-                                var dinfo = createFileInfo(_name, _isDir, _size, _fs_id, _msg_id, frm_arr[index].value, 0);
-                                dirinfo.push(dinfo);
-                                info = info + "\n" + dinfo.getInfo();
-                            }
-                         }
-                        index++;
-                    }
-
-                    console.log( info );
-
-                },
-                error:function(err){
-                    console.log(err);
-                }
-            });
-            //Step3 遍历主目录
-            queryDir(dirinfo, gid.value);// 传gid
+            alert("取得主目录信息成功，可以单独导出该目录下的子目录信息!" );
         }
-        else  if(g_type == 1)
-        {
-            // https://pan.baidu.com/mbox/msg/sessioninfo?to_uk=2181389256&type=2
-            gid_url = "https://pan.baidu.com/mbox/msg/sessioninfo?to_uk=" + frm_arr[0].value + "&type=2";
-          //  alert(gid_url);
-            $.ajax({
-                type:'GET',
-                url: gid_url,
-                data:{},
-                dataType: "json",
-                async: false,
-                success:function(res){
-                    if(res.errno != 0)
-                    {
-                         console.log("errno:" + res.errno + "url: " + gid_url);
-                        return;
-                    }
-                    var rlist = res.records.list;
-                    var obj_length = rlist.length;
-                    var info="" ;
-                    var index = 0;
-                    for(var i = 0 ; i < obj_length ; i++) {
-                        var frm = rlist[i].from_uk;
-                        var to_uk = rlist[i].to_uk;
-                        var _msg_id = rlist[i].msg_id;
-                        var filelist = rlist[i].filelist.list; //注意节点
-                        var len = filelist.length;
-//console.log( "22msg: " + _msg_id );
-                        for(var n = 0 ; n < len ; n++) {
-                            if( selected_arr[index] == "on" )
-                            {//目录被选中才导出
-                                var _name  = filelist[n].server_filename;
-                                var _size  = filelist[n].size;
-                                var _isDir = filelist[n].isdir;
-                                var _fs_id = filelist[n].fs_id;
-                                var dinfo = createFileInfo(_name, _isDir, _size, _fs_id, _msg_id, frm, to_uk); ;
-                                dirinfo.push(dinfo);
-                                info += dinfo.getInfo() + "\n";
-                            }
-                            index++;
-                        }
+  }
 
-                    }
-               //      alert( info );
-                },
-                error:function(err){
-                   console.error(err);
-                }
-            });
-            //Step3 遍历主目录
-            queryDir(dirinfo, 0, 1);
+  function exportSubDir(){
+        let lst = Array.from(document.getElementsByTagName('li')).filter(item => item.getAttribute('node-type') === 'sharelist-item');
+        const obj = lst.filter(item => item.getAttribute('class') === 'on');
+        var obj_length = obj.length;
+        var selected_arr = [];
+        var dirinfo = []; //一级列表数组
+
+        if( g_msgid === null)
+        {
+            alert("请在文件库根目录下执行“获取ID”!" );
+            return;
         }
 
+        for(var i = 0 ; i < obj_length ; i++) {
+            var _name  = $(obj[i]).find('span[class="global-ellipsis sharelist-item-title-name"]').find('a').html();
+            var _size  = 1;
+            var _isDir = 1;
+            var _fs_id = obj[i].getAttribute("data-fid");
+            var dinfo = createFileInfo(_name, _isDir, _size, _fs_id, g_msgid, g_frm, g_to_uk);
+            dirinfo.push(dinfo);
+        }
+
+
+        //Step3 遍历目录
+        queryDir(dirinfo);// 传gid
         var output="";
         for(var n = 0 ; n < dirinfo.length ; n++)
         {
-         //   console.log(dirinfo[n].getAllInfo("") );
+            //   console.log(dirinfo[n].getAllInfo("") );
             output +=dirinfo[n].getAllInfo("", 0) +"\r\n";
         }
 
         output += "<end>";
-
-       var blob = new Blob([output], {type: "text/plain;charset=utf-8"});
+        var blob = new Blob([output], {type: "text/plain;charset=utf-8"});
         saveAs(blob, "exportDirList.txt");
-    }
+     }
 
-    function queryDir(dir_info, gidv, bfirst){
+  function exportDir(){
+        let lst = Array.from(document.getElementsByTagName('li')).filter(item => item.getAttribute('node-type') === 'sharelist-item');
+        const obj = lst.filter(item => item.getAttribute('class') === 'on');
+        var obj_length = obj.length;
+        var selected_arr = [];
+        var dirinfo = []; //一级列表数组
+        g_msgid = null;
+        for(var i = 0 ; i < obj_length ; i++) {
+
+            let lt = Array.from(obj[0].children).filter(item => item.getAttribute('class') === 'sharelist-item-size global-ellipsis');
+            if(lt.length == 1)
+            {
+                if(lt[0].textContent == '-') //文件夹
+                {
+                    var _msgid = obj[i].getAttribute('data-id');
+                    g_msgid = _msgid;
+                    g_gid = obj[i].getAttribute("data-gid");
+                    var _frm = obj[i].getAttribute("data-frm");
+                    var _to_uk = obj[i].getAttribute('data-to');
+                    var _fs_id = obj[i].getAttribute('data-fid');
+
+                    if (_to_uk === null) {
+                        g_type = 2;
+                    } else {
+                        g_type = 1;
+                    }
+
+                    var _name  = $(obj[i]).find('span[class="global-ellipsis sharelist-item-title-name"]').find('a').html();
+                    var _size  = 1;
+                    var _isDir = 1;
+                    var dinfo = createFileInfo(_name, _isDir, _size, _fs_id, _msgid, _frm, _to_uk);
+                    dirinfo.push(dinfo);
+                }
+            }
+        }
+
+        if( g_msgid === null)
+        {
+            alert("请在文件库根目录下执行!" );
+            return;
+        }
+
+
+        //Step3 遍历目录
+        queryDir(dirinfo);
+        var output="";
+        for(var n = 0 ; n < dirinfo.length ; n++)
+        {
+            //   console.log(dirinfo[n].getAllInfo("") );
+            output +=dirinfo[n].getAllInfo("", 0) +"\r\n";
+        }
+
+        output += "<end>";
+        var blob = new Blob([output], {type: "text/plain;charset=utf-8"});
+        saveAs(blob, "exportDirList.txt");
+     }
+
+    function queryDir(dir_info){
       var obj_length = dir_info.length;
 
         for(var i = 0 ; i < obj_length ; i++) {
@@ -221,7 +208,7 @@
                 }
                 else if(g_type == 2)
                 {
-                    dir_url ="https://pan.baidu.com/mbox/msg/shareinfo?msg_id=" + dir_info[i].msg_id + "&page=" + n_page + "&from_uk=" + dir_info[i].from_uk + "&gid=" + gidv +"&type=2&fs_id="+ dir_info[i].fs_id +"&num=100";
+                    dir_url ="https://pan.baidu.com/mbox/msg/shareinfo?msg_id=" + dir_info[i].msg_id + "&page=" + n_page + "&from_uk=" + dir_info[i].from_uk + "&gid=" + g_gid +"&type=2&fs_id="+ dir_info[i].fs_id +"&num=100";
                 }
                 console.log(dir_url);
 
@@ -274,7 +261,6 @@
                         }
                         n_page++;
                         //  alert( info );
-                        //                    if(b_has_dir == 1) queryDir(fileinfo, gidv, 0); //有目录 递归调用
                     },
                     error:function(err){
                         console.error(err);
@@ -283,7 +269,7 @@
                 });
             }
             dir_info[i].sub_file_objs = fileinfo;
-            if(b_has_dir == 1) queryDir(fileinfo, gidv, 0); //有目录 递归调用
+            if(b_has_dir == 1) queryDir(fileinfo); //有目录 递归调用
         }
     }
 
@@ -355,6 +341,7 @@
             { //文件夹
                 this.size=0;
                for(var n = 0 ; n < this.sub_file_objs.length ; n++) {
+
                  this.size += this.sub_file_objs[n].getAllSize();
              }
             }
@@ -366,11 +353,21 @@
 
      //添加导出按钮
     function addButton() {
-         var $dropdownbutton = $('<a class="list-filter" href="javascript:void( 0 );" onclick="exportDirInfo()" node-type="btn_export" title="在文件库根目录，选择要导出的文件夹，点击按钮，清单生成后会提示保存文件。\n如果文件夹内部子文件夹很多，需要等待较长时间。8000个子文件夹大概1小时" style="display: inline;">导出目录信息</a>');
+        var $dropdownbutton = $('<a class="list-filter" href="javascript:void( 0 );" onclick="exportDir()" node-type="btn_export" title="在文件库根目录，选择要导出的文件夹，点击按钮，清单生成后会提示保存文件。\n如果文件夹内部子文件夹很多，需要等待较长时间。8000个子文件夹大概1小时" style="display: inline;">导出目录</a>');
         $('.' + classMap['title']).append($dropdownbutton);
-        $dropdownbutton.click(exportDirInfo);
+        $dropdownbutton.click(exportDir);
+
+        var $dropdownbutton3 = $('<a class="list-filter" href="javascript:void( 0 );" onclick="exportSubDir()" node-type="btn_export" title="导出任意层级的子文件夹信息，但需要先在主目录下，选择文件夹所在根目录，点击<ID按钮>获取主目录信息，然后可以进入子目录，选择要导出的子目录进行导出!" style="display: inline;">子目录</a>');
+        $('.' + classMap['title']).append($dropdownbutton3);
+        $dropdownbutton3.click(exportSubDir);
+
+        var $dropdownbutton2 = $('<a class="list-filter" href="javascript:void( 0 );" onclick="getGID()" node-type="btn_export" title="为了导出子文件夹，需要先获取主目录信息, 请在文件库根目录下，选择要导出的子目录所在的根目录后执行!" style="display: inline;">ID</a>');
+        $('.' + classMap['title']).append($dropdownbutton2);
+        $dropdownbutton2.click(getGID);
+
+
+
     }
     addButton();
-
 //    alert("导出");
 })();
